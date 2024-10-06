2b30d41ba08a3cf2536715a4535ebdfbcc015fec
your name <nguyenkhacnamanh0426@gmail.com>
2024-10-06 17:19:43 +0700

    namanhdaide

--- app/src/main/java/com/example/datn_md03_ungdungmuabangiaysneakzone/MainActivity.java
+++ app/src/main/java/com/example/datn_md03_ungdungmuabangiaysneakzone/MainActivity.java
@@ -20,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
             //may 1
+            // đừng lắm mồm với bổn đế
         });
     }
 }
