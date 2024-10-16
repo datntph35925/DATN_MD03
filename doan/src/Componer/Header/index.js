7d1ff5f9ab4287f2e5cff96e866722b283e4e4a0
TheTai2043 <luonthetai243gmail.com>
2024-10-16 22:12:40 +0700

    iiiii

--- doan/src/Componer/Header/index.js
+++ doan/src/Componer/Header/index.js
@@ -2,11 +2,10 @@ import React from "react";
 
 function Header() {
   return (
-
     <div className="container_header">
-        
+      <h1>dgdgfskfdbksbkjdb</h1>
     </div>
-  )
+  );
 }
 
 export default Header;
